eceda43796f080dcb7f248ac913fc45218212f08
mine0059 <onojefemuemine@gmail.com>
2026-03-20 14:22:30 +0100

    add: google auth env to config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -22,15 +22,17 @@ const config = {
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
     ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as ms.StringValue,
     REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as ms.StringValue,
-    WHITELIST_ADMINS_MAIL: [
-        'oghenemineemma@gmail.com',
-        'onojefemuemine@gmail.com'
-    ],
     defaultResLimit: 20,
     defaultResOffset: 0,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME!,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY!,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
+    GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL!,
+    GOOGLE_ANDROID_CLIENT_ID: process.env.GOOGLE_ANDROID_CLIENT_ID!,
+    GOOGLE_IOS_CLIENT_ID: process.env.GOOGLE_IOS_CLIENT_ID!,
+    WEB_CLIENT_URL: process.env.WEB_CLIENT_URL!
 };
 
 export default config;
